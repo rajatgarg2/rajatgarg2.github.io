3dcba83a54ca61ebcc63900df6722e6314271459
Rajat Garg <68014249+rajatgarg2@users.noreply.github.com>
2020-10-02 22:22:47 +0530

    added rock,paper, and scissor

--- index.js
+++ index.js
@@ -1,3 +1,72 @@
+//Rock Paper and Scissor
+function rpsGame(yourChoice) {
+
+    var humanChoice = yourChoice.id;
+
+    var botChoice = numberToChoice(randToRpsInt());
+
+     result = decideWinner(humanChoice, botChoice);
+
+     message = finalMessage(result);
+
+     rpsFrontEnd(yourChoice.id,botChoice,message); 
+}
+
+function randToRpsInt(){
+    return Math.floor(Math.random()*3)
+}
+
+function numberToChoice(number){
+    return ['rock','paper','scissor'][number]
+}
+
+function decideWinner(yourChoice,computerChoice){
+    var rpsDatabase={
+        'rock': {'rock':0.5 , 'paper':0 , 'scissor':1},
+        'paper':{'rock':1 , 'paper':0.5 , 'scissor':0},
+        'scissor':{'rock':0 , 'paper':1 , 'scissor':0.5}
+    };
+
+    var yourScore = rpsDatabase[yourChoice][computerChoice];
+    var computerScore = rpsDatabase[computerChoice][yourChoice];
+
+    return [yourScore,computerScore];
+}
+
+function finalMessage([yourScore,computerScore]){
+    if(yourScore === 0)
+        return {'message':'You Lost!','color':'red'};
+    else if(yourScore === 0.5)
+        return {'message':'You Tied!','color':'yellow'};
+    else
+        return {'message':'You Won!','color':'green'};
+}
+
+function rpsFrontEnd(humanImageChoice,computerImageChoice,finalMessage){
+    var imageDatabase={
+        'rock':document.getElementById('rock').src,
+        'paper':document.getElementById('paper').src,
+        'scissor':document.getElementById('scissor').src,
+    };
+
+    document.getElementById('rock').remove();
+    document.getElementById('paper').remove();
+    document.getElementById('scissor').remove();
+
+    var humanDiv = document.createElement('div');
+    var botDiv = document.createElement('div');
+    var messageDiv = document.createElement('div');
+
+    humanDiv.innerHTML = "<img src='"+ imageDatabase[humanImageChoice] +"' style='box-shadow:0px 10px 50px rgba(37, 50, 233, 1)'>"
+    messageDiv.innerHTML = "<h1 style='color: " + finalMessage['color'] + "; font-size: 60px; padding: 30px;'>" + finalMessage['message'] + "</h1>"
+    botDiv.innerHTML = "<img src='"+ imageDatabase[computerImageChoice] +"' style='box-shadow:0px 10px 50px rgba(250, 0, 0, 1);'>"
+    document.getElementById("flex-box-rps-div").appendChild(humanDiv);
+    document.getElementById("flex-box-rps-div").appendChild(messageDiv);
+    document.getElementById("flex-box-rps-div").appendChild(botDiv);
+}
+
+
+
 //Blackjack!
 let blackjackGame = {
     'you': {'scoreSpan': '#your-bj-result', 'div': '#yourBox', 'score': 0},
